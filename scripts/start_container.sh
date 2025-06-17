f420ef4b75181a7033631f258a57b34eb46a8b62
Savithri Marati <savithri62001@gmail.com>
2025-06-17 18:07:17 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull savithri06/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 savithri06/simple-python-flask-app
+docker run -d -p 5001:5000 savithri06/simple-python-flask-app
 
